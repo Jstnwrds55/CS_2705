ad675daef7712034c4f0df9070ef304abd3049e3
Justin <jstnwrds55@gmail.com>
2018-01-09 18:40:09 -0700

    renamed file and finished the assignment

---
+++ JustinEdwardsHelloWorld.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+
+firstName = input("Please enter your first name: ")
+lastName = input("Please enter your last name: ")
+fullName = firstName + " " + lastName
+print("Your whole name is", fullName + ", as if you didn't already know that...")
+
+num1 = float(input("Enter the first decimal number you want to add: "))
+num2 = float(input("Now enter the second decimal number you want to add: "))
+numSum = num1 + num2
+
+print("The sum of those numbers is", numSum)
