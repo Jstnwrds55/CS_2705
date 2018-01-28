4fa56f5e94e1b770196223172b7cd534ed2d9dae
Justin <jstnwrds55@gmail.com>
2018-01-28 11:25:13 -0700

    finished subnet lab

--- JustinEdwardsIPLab.py
+++ JustinEdwardsIPLab.py
@@ -6,24 +6,35 @@ def ipAddress(ip, outputs):
     ipInterface = ipaddress.ip_interface(ip)
     ipNetwork = ipInterface.network
 
+    print(ipNetwork)
+
     if outputs == 'networkFirstUsable':
         print('What is the first address of a block of classless addresses if one of the addresses is ', ip + '?')
-        print('{} is the network address'.format(ipaddress.ip_network(ipNetwork).network_address))
-        print('{} is the first usable address'.format((ipaddress.ip_network(ipNetwork).network_address) + 1), '\n')
+        print('Network Address: {}'.format(ipaddress.ip_network(ipNetwork).network_address))
+        print('First Usable Address: {}'.format((ipaddress.ip_network(ipNetwork).network_address) + 1), '\n')
 
     elif outputs == 'totalUsable':
         print('Find the number of addresses in a block of classless addresses if one of the addresses is', ip + '.')
-        print('{} is the total number of addresses available'.format(ipaddress.ip_network(ipNetwork).num_addresses))
-        print('{} is the total number of usable addresses'.format((ipaddress.ip_network(ipNetwork).num_addresses) - 2),
+        print('Total Addresses Available: {}'.format(ipaddress.ip_network(ipNetwork).num_addresses))
+        print('Total Usable Addresses: {}'.format((ipaddress.ip_network(ipNetwork).num_addresses) - 2),
               '\n')
 
     elif outputs == 'broadcastUsable':
         print('What is the last address of a block of classless addresses if one of the addresses is', ip + '?')
-        print('{} is the broadcast address'.format(ipaddress.ip_network(ipNetwork).broadcast_address))
-        print('{} is the last usable address'.format((ipaddress.ip_network(ipNetwork).broadcast_address) - 1), '\n')
+        print('Broadcast Address: {}'.format(ipaddress.ip_network(ipNetwork).broadcast_address))
+        print('Last Usable Address: {}'.format((ipaddress.ip_network(ipNetwork).broadcast_address) - 1), '\n')
 
     elif outputs == "prefixLength":
-        print(list(ipaddress.ip_network(ipNetwork).subnets(prefixlen_diff=2)))
+        print('An organization is granted a block; one address is', ip +'. The organization needs 10 subnets. What is the subnet prefix length?')
+        print('Prefix Length: {}\n'.format(ipaddress.ip_network(ipNetwork).prefixlen))
+
+    elif outputs == 'maxSubAddress':
+        print('An organization is granted a block; one address is 110.10.10.64/25. If the subnet prefix length is /28, what is the maximum number of subnets and how many addresses in each subnet?')
+        print('{} subnets with {} addresses in each subnet.'.format(2*2*2, ipaddress.ip_network(ipNetwork).num_addresses), '\n')
+
+    elif outputs == 'totalAddress':
+        print('An organization is granted a block of classless addresses with the starting address: ' + ip + '. How many addresses are granted?')
+        print('Total Addresses Available: {}'.format(ipaddress.ip_network(ipNetwork).num_addresses), '\n')
 
 
 ipAddress('192.168.2.76/28', 'networkFirstUsable')
@@ -35,6 +46,14 @@ ipAddress('101.10.2.8/29', 'totalUsable')
 
 ipAddress('192.168.2.137/27', 'broadcastUsable')
 ipAddress('110.10.2.55/30', 'broadcastUsable')
+
+ipAddress('110.10.10.64/24', 'prefixLength')
+
+ipAddress('110.10.10.64/28', 'maxSubAddress')
+
+ipAddress('156.78.51.24/25', 'totalAddress')
+ipAddress('156.78.51.24/30', 'totalAddress')
+ipAddress('166.25.132.0/3', 'totalAddress')
 
 
 
